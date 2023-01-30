055feb2caec18bfc19cca6be29aba78de31e2ff7
ELS <57393100+kodosa7@users.noreply.github.com>
2023-01-30 10:53:31 +0100

    refactored code

--- App.jsx
+++ App.jsx
@@ -7,38 +7,16 @@ export default function App() {
     const [questions, setQuestions] = React.useState([])
 
     // get 5 random questions from an API
-    function getQuestionsFromAPI() {
-        React.useEffect(() => {
-            console.log("useEffect ran")
-            fetch('https://opentdb.com/api.php?amount=5')
-                .then(res => res.json())
-                .then(data => setQuestions(data.results))
-            }, [])
-    }
+    React.useEffect(() => {
+        console.log("useEffect ran")
+        fetch('https://opentdb.com/api.php?amount=5')
+            .then(res => res.json())
+            .then(data => setQuestions(data.results))
+        }, [])
     
-    
-    // const data = getQuestionsFromAPI()
-    
-    function renderQuestions() {
-        getQuestionsFromAPI()
-        console.log("renderQuestions launched")
-        console.log("all questions are:", questions)
-
-        if (!questions.length) return <div>Loading questions...</div>
-
-        return questions.map((question, index) => (
-            <div key={question.id}>
-                <p>Question {index + 1}: {question.question}</p>
-                <p>Correct answer: {question.correct_answer}</p>
-                <p>Incorrect: {question.incorrect_answers.join(", ")}</p>
-            </div>
-        ))
-    }
-    
-    renderQuestions()
 
     function checkAllAnswers() {
-
+        // handle the check button here
     }
 
     return (
