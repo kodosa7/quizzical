3c20572ec54d1e5f7dcb9b5465fd3b7461b4e565
ELS <57393100+kodosa7@users.noreply.github.com>
2023-02-02 13:08:11 +0100

    implemented all-answers-buttons check

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,34 @@
 import React from 'react'
 import FrontPage from './components/FrontPage'
-import Questions from './components/Questions'
+import Question from './components/Question'
 import './App.css'
 
 export default function App() {
     const [questions, setQuestions] = React.useState([])
+    const [clickedAll, setClickedAll] = React.useState([
+        {
+            "1": false,
+            "2": false,
+            "3": false,
+            "4": false,
+            "5": false
+        }
+    ])
 
     // get 5 random questions from an API
-    function getQuestionsFromAPI() {
-        React.useEffect(() => {
-            console.log("useEffect ran")
-            fetch('https://opentdb.com/api.php?amount=5')
-                .then(res => res.json())
-                .then(data => setQuestions(data.results))
-            }, [])
-    }
+    React.useEffect(() => {
+        fetch('https://opentdb.com/api.php?amount=5')
+            .then(res => res.json())
+            .then(data => setQuestions(data.results))
+        }, [])
     
-    
-    // const data = getQuestionsFromAPI()
-    
-    function renderQuestions() {
-        getQuestionsFromAPI()
-        console.log("renderQuestions launched")
-        console.log("all questions are:", questions)
-
-        if (!questions.length) return <div>Loading questions...</div>
-
-        return questions.map((question, index) => (
-            <div key={question.id}>
-                <p>Question {index + 1}: {question.question}</p>
-                <p>Correct answer: {question.correct_answer}</p>
-                <p>Incorrect: {question.incorrect_answers.join(", ")}</p>
-            </div>
-        ))
-    }
-    
-    renderQuestions()
 
     function checkAllAnswers() {
-
+        console.log("button clicked")
     }
+
+    // check if buttons of all answers have been clicked
+    const haveAllBtnsClicked = Object.values(clickedAll[0]).every((value, i, array) => array[i] === true)
 
     return (
         <div className="App">
@@ -47,16 +36,20 @@ export default function App() {
                 {/* <FrontPage /> */}
                 {questions.map((question, index) => (
                     <div key={question.id}>
-                        <Questions
+                        <Question
                             id={index + 1}
                             question={question.question}
                             correct={question.correct_answer}
                             incorrect={question.incorrect_answers}
+                            clickedAll={clickedAll} setClickedAll={setClickedAll}
                         />
                     </div>  
                 ))}
-
-                <button className="check-btn" onClick={checkAllAnswers}>Check answers</button>
+                <button
+                    onClick={checkAllAnswers}
+                    className={`check-btn ${haveAllBtnsClicked ? "" : "greyed"}`}
+                >Check answers
+                </button>
             </div>
         </div>
     )
---
+++ src/components/Question.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react"
+
+export default function Question(props) {
+    // const [isShuffled, setIsShuffled] = useState(true)  // added state to keep track of shuffling
+    const [isPressed, setIsPressed] = useState({})  // added state
+
+    useEffect(() => {
+        setIsPressed({
+            [props.correct]: true
+        })
+    }, [])
+
+    function unshuffledAnswersObject() {
+        const allAnswersObject = {}
+        allAnswersObject[props.correct] = true
+        props.incorrect.map(item => 
+            allAnswersObject[item] = false
+        )
+        return allAnswersObject
+    }
+
+    // *** commented code is for shuffling the resulted array
+    const shuffledAnswersObject = unshuffledAnswersObject()
+    // let entries = Object.entries(unshuffledAnswersObject())
+    // if (isShuffled) {
+        // entries = entries.sort(() => Math.random() - 0.5)  // shuffle entries randomly
+    // }
+    // const shuffledAnswersObject = Object.fromEntries(entries)
+    // console.log("shuffledAnswersObject", shuffledAnswersObject)
+
+
+    function checkButton(e) {
+        let answer = e.target.innerHTML
+        setIsPressed({
+            [answer]: true
+        })
+        console.log(props.id)
+
+        props.setClickedAll(prevClickedAll => {
+            const updatedClickedAll = { ...prevClickedAll[0] }
+            updatedClickedAll[props.id] = true
+            // console.log("updatedClickedAll", updatedClickedAll)
+            const allBtnsClicked = Object.values(updatedClickedAll).every(value => value === true)
+            // console.log("allClicked", allBtnsClicked)
+            return [updatedClickedAll, allBtnsClicked]
+        })
+
+        console.log("clicked answer:", answer)
+        console.log("correct answer:", props.correct)
+        console.log("clickedAll", props.clickedAll)
+        
+        if (props.correct === answer) {
+            console.log("*** CORRECT ANSWER!")
+        } else {
+            console.log("!!! WRONG ANSWER")
+        }
+        // setIsShuffled(false)  // set shuffled to false when button is clicked
+    }
+
+
+    return (
+        <div className="questions-container">
+            <div className="questions">
+                <h3 dangerouslySetInnerHTML={{ __html: props.question }}></h3>
+                {Object.entries(shuffledAnswersObject).map((item, index) => 
+                    <button
+                        onClick={checkButton}
+                        className={`answer-btn ${isPressed[item[0]] ? "pressed" : ""}`}  // added class to change color
+                        key={index}
+                        dangerouslySetInnerHTML={{ __html: item[0]}}
+                    ></button>)}
+                <div className="line"></div>
+            </div>
+        </div>
+    )
+}
